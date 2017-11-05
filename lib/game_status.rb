59efd820279889163df3f39bbd2f431d3e34646d
Crista Cooper <aine3d@gmail.com>
2017-11-05 18:52:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,6 +30,8 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
+      @win_combo = []
+      @win_combo << win_combination
     end
   end
   false
@@ -57,6 +59,7 @@ end
 
 def winner(board)
 
-  
+  if won?(board) == true
+    
 
 end
